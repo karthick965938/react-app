4b4961f2f8a2a12c882fedbf7c2ccebf134203c8
Karthick nagarajan <karthick965938@gmail.com>
2019-09-17 19:50:04 +0530

    Added SWAPI and implement some basic plugins

--- src/components/IndividualFilm.js
+++ src/components/IndividualFilm.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../App.css';
 import {
   Form, Input, Navbar, Nav,
   NavbarBrand, NavLink, NavItem, UncontrolledDropdown,
@@ -9,33 +9,25 @@ import {
   CardSubtitle, CardBody
 } from 'reactstrap';
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHeart, faEnvelope, faKey } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import swal from 'sweetalert';
-library.add(faHeart, faEnvelope, faKey);
-
-
-export default class App extends React.Component {
-
+export default class IndividualFilm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       error: null,
       isLoaded: false,
-      filter: "",
-      items: []
+      data: []
     };
   }
 
   componentDidMount() {
-    fetch("https://swapi.co/api/films/")
+    console.log(this)
+    fetch("https://swapi.co/api/films/"+this.props.match.params.id)
       .then(res => res.json())
       .then(
         (result) => {
           this.setState({
             isLoaded: true,
-            items: result.results
+            data: result
           });
         },
         (error) => {
@@ -47,37 +39,8 @@ export default class App extends React.Component {
       )
   }
 
-
-  changePage = film => {
-    // film
-    var str = film.url
-    var get_url = str.split('https://swapi.co/api');
-    console.log(film.url)
-    this.props.history.push(get_url[1])
-  };
-
-  doFavorite = film => {
-    swal({
-      title: "Added to favorite!",
-      text: "This film added to your favorite list!",
-      icon: "success",
-    });
-  };
-
-
-  handleChange = event => {
-    this.setState({ filter: event.target.value });
-  };
-
   render() {
-    const { error, filter, isLoaded, items } = this.state;
-    const lowercasedFilter = filter.toLowerCase();
-    const filteredData = items.filter(item => {
-      return Object.keys(item).some(key =>
-        // console.log(item[key])
-        item[key].toLowerCase().includes(lowercasedFilter)
-      );
-    });
+    const { error, isLoaded, data } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
@@ -87,44 +50,47 @@ export default class App extends React.Component {
         <Fragment>
           <header>
             <Navbar fixed="top" color="light" light expand="xs" className="border-bottom border-gray bg-white" style={{ height: 80 }}>
+            
               <Container>
                 <Row noGutters className="position-relative w-100 align-items-center">
+                
                   <Col className="d-none d-lg-flex justify-content-start">
                     <Nav className="mrx-auto" navbar>
+                    
                       <NavItem className="d-flex align-items-center">
                         <NavLink className="font-weight-bold" href="/">
                           <img src={logo} alt="avatar" className="img-fluid rounded-circle" style={{ width: 36 }} />
                         </NavLink>
                       </NavItem>
+                      
                       <NavItem className="d-flex align-items-center">
                         <NavLink className="font-weight-bold" href="/">Home</NavLink>
                       </NavItem>
                     </Nav>
                   </Col>
+                  
                   <Col className="d-none d-lg-flex justify-content-end">
                     <Form inline>
                       <Input type="search" className="mr-3" placeholder="Search Films" />
                     </Form>
                   </Col>
+                  
                 </Row>
               </Container>
+              
             </Navbar>
           </header>
-
           <main className="my-5 py-5">
             <Container className="px-0">
               <Row>
-                {items.map(film => (
-                  <Col xs="6" sm="3">
-                    <Card style={{cursor: 'pointer'}}>
-                      <CardBody onClick={()=>this.changePage(film)}>
-                        <CardTitle><b>{film.title}</b></CardTitle>
-                        <CardText>{film.opening_crawl}</CardText>
-                      </CardBody>
-                      <Button outline color="success" onClick={()=>this.doFavorite(film)}><FontAwesomeIcon icon="heart"/></Button>
-                    </Card>
-                  </Col>
-                ))}
+                <Col>
+                  <Card>
+                    <CardBody>
+                      <CardTitle><b>{data.title}</b></CardTitle>
+                      <CardText>{data.opening_crawl}</CardText>
+                    </CardBody>
+                  </Card>
+                </Col>
               </Row>
             </Container>
           </main>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// Components
+import IndividualFilm from './components/IndividualFilm';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <Route path="/" exact component={App} />
+    <Route path="/films/:id" exact component={IndividualFilm} />
+  </Router>
+)
+ReactDOM.render(routing, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
